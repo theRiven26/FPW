66059f316d56ae4902d7eb7ea7f8b6e8cb2a31b8
Anrew Krivenko <demize26rus@gmail.com>
2023-08-29 20:57:34 +0300

    Добавьте файлы проекта.

---
+++ FPW/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FPW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
